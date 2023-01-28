62524900a03f4b35abe0883147154595ec9e4d62
misaki arita <misaki.arita0838@gmail.com>
2023-01-28 16:19:18 +0900

    init unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "https://github.com/misakiarita/aws_exam.git"
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/aws_exam'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
